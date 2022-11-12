45e5a96cb476094f7a75bb3decb76b062e08a828
ADL1337 <49399494+ADL1337@users.noreply.github.com>
2022-11-12 19:06:25 +0100

    Working commit

---
+++ YouTubetoMP3.py
@@ -0,0 +1,21 @@
+if __name__ == "__main__":
+    from pytube import Playlist
+    import os
+    cwd = os.getcwd()
+
+    downloadPath = cwd + "/Downloads"
+    playlist = Playlist(input("Enter a YouTube playlist: "))
+
+    for video in playlist.videos:
+        try:
+            title = video.title
+            if os.path.exists(downloadPath + "/" + title + ".mp3"):
+                print(f"Download for {title} failed. File already exists")
+            else:
+                out_file = video.streams.filter(only_audio=True).first().download(output_path=downloadPath)
+                name, ext = os.path.splitext(out_file)
+                new_file = name + ".mp3"
+                os.rename(out_file, new_file)
+                print(f"Download for {title} completed successfully.")
+        except:
+            print(f"Download for {title} failed.")
